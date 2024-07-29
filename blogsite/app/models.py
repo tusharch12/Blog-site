9d278e1ec5f3b6d1a268e4faa3120064473f3acb
tusharch12 <tusharch6577@gmail.com>
2024-07-29 17:02:20 +0530

    update + add modal

--- blogsite/app/models.py
+++ blogsite/app/models.py
@@ -7,12 +7,18 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_draft = models.BooleanField(default = False)
     datetime_first = models.DateTimeField(auto_now=True)
     body = RichTextUploadingField(default="content")
 
+    class Meta:
+        ordering = ['-datetime_first']
+
 
     def __str__(self):
-        return  self.title +' | ' + str(self.author)
+        if(self.is_draft):
+            return  self.title +' | ' + str(self.author) +' | Draft | ' + self.datetime_first.strftime("%d/%m/%Y")
+        return  self.title +' | ' + str(self.author) + ' | ' + self.datetime_first.strftime("%d/%m/%Y")
 
 class BlogMedia(models.Model):
     blog = models.ForeignKey(Post,on_delete=models.CASCADE, related_name="media")
@@ -20,6 +26,18 @@ class BlogMedia(models.Model):
 
     def __str__(self):
         return self.blog.title
+        
+class BlogComment(models.Model):
+    blog = models.ForeignKey(Post,on_delete=models.CASCADE, related_name='comment')
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    comment = models.TextField()  
+
+    class Meta:
+        ordering = ['-id']
+
+    # def __str__(self):
+    #     return self.name | self.comment
 
 
         
